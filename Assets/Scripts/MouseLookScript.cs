b7d20fe34accd51b76433530f08e89784ae9c673
Artem <tumofeu@gmail.com>
2017-01-11 16:57:14 +0400

    Implement character movement

--- Assets/Scripts/MouseLookScript.cs
+++ Assets/Scripts/MouseLookScript.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class MouseLookScript : MonoBehaviour {
+public class MouseLookScript : MonoBehaviour
+{
     public float sensitivityX = 2;
     public float sensitivityY = 2;
 
@@ -18,20 +19,21 @@ public class MouseLookScript : MonoBehaviour {
     public float frameCounter = 20;
 
     Quaternion originalRotation;
+    public GameObject player;
 
     void Update()
     {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
 
         rotationY = ClampAngle(rotationY, minimumY, maximumY);
         rotationX = ClampAngle(rotationX, minimumX, maximumX);
 
-            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.left);
-            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.left);
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
 
-            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
-        
+        transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+
     }
 
     public static float ClampAngle(float angle, float min, float max)
@@ -49,6 +51,11 @@ public class MouseLookScript : MonoBehaviour {
             }
         }
         return Mathf.Clamp(angle, min, max);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + new Vector3(0, 1f, 0);
     }
 
     void Start()
---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public Camera playerCamera;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(speed * GetMovementVector(playerCamera.transform.forward));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(speed * GetMovementVector(playerCamera.transform.forward * -1));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(speed * GetMovementVector(playerCamera.transform.right * -1));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * GetMovementVector(playerCamera.transform.right));
+        }
+    }
+    Vector3 GetMovementVector(Vector3 direction)
+    {
+        return Vector3.ProjectOnPlane(direction, new Vector3(0, 1, 0)).normalized;
+    }
+}
