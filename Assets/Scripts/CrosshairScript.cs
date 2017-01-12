1ff869d4b2fcc611b6db47249d2e76367d399b70
Artem <tumofeu@gmail.com>
2017-01-12 14:29:47 +0400

    Add crosshair

---
+++ Assets/Scripts/CrosshairScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairScript : MonoBehaviour
+{
+    public Texture crosshairTexture;
+    Rect rectangle;
+
+    // Use this for initialization
+    void Start()
+    {
+        var crosshairSize = Screen.width * 0.02f;
+        rectangle = new Rect(
+            Screen.width / 2f - crosshairSize / 2,
+            Screen.height / 2f - crosshairSize / 2,
+            crosshairSize,
+            crosshairSize);
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(rectangle, crosshairTexture);
+    }
+}
